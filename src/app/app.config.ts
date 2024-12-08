d7fdb72e064b62b3399470209bb67c2f98fd5f89
Kevin Ekhorutomwen <kevin.ekhorutomwen@yahoo.de>
2024-12-08 15:56:27 +0100

    initialize firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"delivery-49235","appId":"1:1024581359943:web:ef6270afecae3f67359462","storageBucket":"delivery-49235.firebasestorage.app","apiKey":"AIzaSyDz1nagAo835o3Qcb0ix95GtA_bNmQpxKA","authDomain":"delivery-49235.firebaseapp.com","messagingSenderId":"1024581359943","measurementId":"G-LQD54DQRK6"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), providePerformance(() => getPerformance())]
 };
