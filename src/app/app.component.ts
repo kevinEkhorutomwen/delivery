39205f4ae1a876acb255126089498c7f208c623e
Kevin Ekhorutomwen <kevin.ekhorutomwen@yahoo.de>
2024-12-09 08:30:52 +0100

    added Firebase Auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Auth, createUserWithEmailAndPassword } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,18 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  
+  constructor(private auth: Auth) {
+    this.registerUser("Kevin.Ekhorutomwen@yahoo.de", "Qweasd123");
+  }
+
+  async registerUser(email: string, password: string) {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(this.auth, email, password);
+      console.log('User created:', userCredential.user);
+    } catch (error) {
+      console.error('Error during registration:', error);
+    }
+  }
   title = 'delivery';
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+    firebase: {
+        appId: "1:1024581359943:web:ef6270afecae3f67359462",
+        authDomain: "delivery-49235.firebaseapp.com",
+        projectId: "delivery-49235",
+        storageBucket: "delivery-49235.firebasestorage.app",
+        messagingSenderId: "1024581359943",
+        apiKey: "AIzaSyDz1nagAo835o3Qcb0ix95GtA_bNmQpxKA",
+        measurementId: "G-LQD54DQRK6"
+      },
+      production: true,
+};
