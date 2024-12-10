3f04df49970e8fe07a481bab8aa98c95d43cf918
Kevin Ekhorutomwen <kevin.ekhorutomwen@yahoo.de>
2024-12-10 12:44:08 +0100

    added Prdouct Detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { adminGuard } from './core/guards/admin.guard';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ShopComponent } from './pages/shop/shop.component';
+import { ProductDetailsComponent } from './pages/shop/product-details/product-details.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'shop', component: ShopComponent},
+    {path: 'shop/:id', component: ProductDetailsComponent},
     {path: 'account', loadChildren: () => import('./pages/account/routes').then(r => r.accountRoutes)},
     {path: 'admin', loadComponent: () => import('./pages/admin/admin.component')
         .then(c => c.AdminComponent), canActivate: [authGuard, adminGuard]},
